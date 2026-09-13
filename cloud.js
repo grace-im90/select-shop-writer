2689116739f72e6949be02be44d4db913ec62ca7
grace-im90 <imeunchong@gmail.com>
2026-09-13 17:53:32 +0900

    Fix cloud loader script tags

--- cloud.js
+++ cloud.js
@@ -2,7 +2,7 @@
   var src = (document.currentScript && document.currentScript.src) || "";
   var base = src ? src.replace(/\/[^\/]*$/, "") : "https://grace-im90.github.io/select-shop-writer";
   function writeScript(path) {
-    document.write('<script src="' + path + '"><\\/script>');
+    document.write('<script src="' + path + '"><\/script>');
   }
   writeScript(base + "/cloud-core.js?v=1");
   writeScript(base + "/cloud-patches.js?v=1");
