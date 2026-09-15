c1835ecb5eef3f9f594a2511fc263ddbe025f0a1
grace-im90 <imeunchong@gmail.com>
2026-09-16 01:11:32 +0900

    Bump AI prefill patch cache version

--- cloud.js
+++ cloud.js
@@ -5,6 +5,6 @@
     document.write('<script src="' + path + '"><\/script>');
   }
   writeScript(base + "/cloud-core.js?v=1");
-  writeScript(base + "/cloud-patches.js?v=3");
+  writeScript(base + "/cloud-patches.js?v=4");
   if (/\/products\/?$/.test(location.pathname)) writeScript("management.js?v=9");
 })();
