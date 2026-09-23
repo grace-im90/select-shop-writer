74c9283ab1cdedf6ab4e3bea652f249bcc3029ab
grace-im90 <imeunchong@gmail.com>
2026-09-24 03:17:40 +0900

    Refresh cached cloud scripts

--- cloud.js
+++ cloud.js
@@ -4,7 +4,7 @@
   function writeScript(path) {
     document.write('<script src="' + path + '"><\/script>');
   }
-  writeScript(base + "/cloud-core.js?v=1");
+  writeScript(base + "/cloud-core.js?v=2");
   writeScript(base + "/cloud-patches.js?v=6");
   if (/\/products\/?$/.test(location.pathname)) writeScript("management.js?v=9");
 })();
